e41d7683ea17941db885b39b63a88d33d350d260
Mathieu Leonardon <mathieu.leonardon@imt-atlantique.fr>
2024-01-31 16:18:21 +0100

    Able to change number of queries and shots.

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import torch
 from PIL import Image
 import torchvision.transforms as transforms
 import os
-import matplotlib
-import matplotlib.pyplot as plt
-from utils import visualize_few_shot_space
+# from utils import visualize_few_shot_space
 from utils import hover_few_shot_space
+import argparse
 
-shots = 18
-queries = 2
+# download images from https://partage.imt.fr/index.php/s/EJagF3c5pa22C4M
+#!wget https://partage.imt.fr/index.php/s/EJagF3c5pa22C4M/download
+
+
+# parse shots and queries
+parser = argparse.ArgumentParser()
+parser.add_argument('--nb_ways', type=int, default=3)
+parser.add_argument('--nb_shots', type=int, default=18)
+parser.add_argument('--nb_queries', type=int, default=2)
+args = parser.parse_args()
+
 
 # torch.set_default_device('cuda')
 
@@ -22,11 +30,6 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 ])
-
-
-
-i = 0
-inputs = []
 
     # def get_class_images(class_name):
     #     class_images = []
@@ -74,23 +77,32 @@ class_k_features = norm_center_norm(class_k_features)
 class_p_features = norm_center_norm(class_p_features)
 class_r_features = norm_center_norm(class_r_features)
 
+# only keep shots + queries
+class_f_features = class_f_features[0:args.nb_shots + args.nb_queries]
+class_k_features = class_k_features[0:args.nb_shots + args.nb_queries]
+class_p_features = class_p_features[0:args.nb_shots + args.nb_queries]
+class_r_features = class_r_features[0:args.nb_shots + args.nb_queries]
+
+
 # Nearest Class Mean
-class_f_mean = class_f_features[0:shots-1].mean(dim=0)
-class_k_mean = class_k_features[0:shots-1].mean(dim=0)
-class_p_mean = class_p_features[0:shots-1].mean(dim=0)
-class_r_mean = class_r_features[0:shots-1].mean(dim=0)
+class_f_mean = class_f_features[0:args.nb_shots].mean(dim=0)
+class_k_mean = class_k_features[0:args.nb_shots].mean(dim=0)
+class_p_mean = class_p_features[0:args.nb_shots].mean(dim=0)
+class_r_mean = class_r_features[0:args.nb_shots].mean(dim=0)
 
 features = [class_f_features, class_k_features, class_p_features, class_r_features]
+
+
 
 c = 0
 for feature in features :
     print('Class number: ' + str(c))
-    for q in range(queries):
+    for q in range(args.nb_queries):
         print('Query number: ' + str(q))
-        print('Distance with f centroid is: ', torch.norm(feature[shots + q]- class_f_mean, dim=-1))
-        print('Distance with k centroid is: ', torch.norm(feature[shots + q]- class_k_mean, dim=-1))
-        print('Distance with p centroid is: ', torch.norm(feature[shots + q]- class_p_mean, dim=-1))
-        print('Distance with r centroid is: ', torch.norm(feature[shots + q]- class_r_mean, dim=-1))
+        print('Distance with f centroid is: ', torch.norm(feature[args.nb_shots + q]- class_f_mean, dim=-1))
+        print('Distance with k centroid is: ', torch.norm(feature[args.nb_shots + q]- class_k_mean, dim=-1))
+        print('Distance with p centroid is: ', torch.norm(feature[args.nb_shots + q]- class_p_mean, dim=-1))
+        print('Distance with r centroid is: ', torch.norm(feature[args.nb_shots + q]- class_r_mean, dim=-1))
 
     c = c + 1
 
@@ -102,14 +114,14 @@ def reduce_dimension(feats):
     """
         Perfom QR decomposition on features
         - In:
-            * feats : torch.Tensor with features of size [ARGS.nb_ways, ARGS.nb_shots+ARGS.nb_queries, 1024] ([3, 10, 1024])
+            * feats : torch.Tensor with features of size [args.nb_ways, args.nb_shots+args.nb_queries, 1024] ([3, 10, 1024])
         - Out:
-            * reduced_features : torch.Tensor of dimension [ARGS.nb_ways, ARGS.nb_shots+ARGS.nb_queries, ARGS.nb_ways-1] ([3, 10 , 2])
+            * reduced_features : torch.Tensor of dimension [args.nb_ways, args.nb_shots+args.nb_queries, args.nb_ways-1] ([3, 10 , 2])
     """
     dim = feats.shape[-1] # 1024
 
     # Compute the centroids of each class
-    supports = feats[:, :shots]
+    supports = feats[:, :args.nb_shots]
     means = torch.mean(supports, dim=1)
 
     # Get Two directions of the centroids.
@@ -127,17 +139,6 @@ reduced_features = reduce_dimension(feats)
 print(reduced_features.shape)
 
 
-# reduced_features = reduced_features.cpu().detach().numpy()
-print(reduced_features.shape)
-
-
-ARGS = lambda: None
-ARGS.nb_ways = 3
-ARGS.nb_queries = queries
-ARGS.nb_shots = shots
-
-print(ARGS.nb_queries)
-
 run_classes = [0,1,2]
 run_indices = [list(range(20)),list(range(20)),list(range(20))]
 run_classes = torch.tensor(run_classes)
@@ -146,6 +147,7 @@ run_indices = torch.tensor(run_indices)
 # remove grad from reduced_features
 reduced_features = reduced_features.detach()
 
-visualize_few_shot_space(reduced_features, run_classes, run_indices, ARGS)
+print("reduced features: ", reduced_features.shape)
+# visualize_few_shot_space(reduced_features, run_classes, run_indices, args)
 
-hover_few_shot_space(reduced_features, run_classes, run_indices, ARGS, images_path="images/")
+hover_few_shot_space(reduced_features, run_classes, run_indices, args, images_path="images/")
--- utils.py
+++ utils.py
@@ -1,41 +1,33 @@
-import argparse
 import torch
 import torch.nn.functional as F
 import numpy as np
-import random
-import matplotlib
 import matplotlib.pyplot as plt
-import tqdm
-import ast
 import os
-import sys
 import csv
 import torchvision.transforms
 from PIL import Image
 from IPython.core.display import display, HTML
-import sklearn.metrics
-import scipy.cluster.hierarchy as hierarchy
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import warnings
 warnings.filterwarnings("ignore")
 
-def compute_hyperplans_and_grid(feats, ARGS, crops=False):
+def compute_hyperplans_and_grid(feats, args, crops=False):
     """
         Given features, compute the hyperplans between each class and returns the a grid of predictions.
         - In:
             * feats : Feature tensor with lower dimension (Here dim=2).
-            * ARGS : args parser object.
+            * args : args parser object.
             * crops : Handle multiple crops per image. For now set it to False
         - Out:
             * plot_grid : A dictionnary with necessary
     """
     assert feats.shape[-1]==2, 'Error, features should be of dimension 2'
-    feats = feats[:, :ARGS.nb_shots]
+    feats = feats[:, :args.nb_shots]
     if crops: feats = torch.mean(feats, dim=2)
 
     grid_size=100
-    x_min, x_max = feats.reshape(-1, ARGS.nb_ways-1).min(dim=0)[0][0].item(), feats.reshape(-1, ARGS.nb_ways-1).max(dim=0)[0][0].item()
-    y_min, y_max = feats.reshape(-1, ARGS.nb_ways-1).min(dim=0)[0][1].item(), feats.reshape(-1, ARGS.nb_ways-1).max(dim=0)[0][1].item()
+    x_min, x_max = feats.reshape(-1, args.nb_ways-1).min(dim=0)[0][0].item(), feats.reshape(-1, args.nb_ways-1).max(dim=0)[0][0].item()
+    y_min, y_max = feats.reshape(-1, args.nb_ways-1).min(dim=0)[0][1].item(), feats.reshape(-1, args.nb_ways-1).max(dim=0)[0][1].item()
 
     xmingrid = x_min-abs(x_min)*0.4
     ymingrid = y_min-abs(y_min)*0.4
@@ -52,7 +44,7 @@ def compute_hyperplans_and_grid(feats, ARGS, crops=False):
     Z = Z.reshape(grid_size, grid_size)
 
     hyperplans = []
-    for n in range(-1, ARGS.nb_ways-1):
+    for n in range(-1, args.nb_ways-1):
         x1 = means[n].cpu() # centroid class 2
         x2 = means[n+1].cpu() # centroid class 2
         middle = (x1+x2)/2
@@ -84,99 +76,6 @@ def compute_hyperplans_and_grid(feats, ARGS, crops=False):
                 'intersection':intersection}
 
     return plot_grid
-
-def visualize_few_shot_space(reduced_features, run_classes, run_indices, ARGS, query_start=0, query_end=2, plot_support=True, crops=False, figsize=(25, 15)):
-    """
-        Visualize the feature space. Allows to plot multiple queries at the same time.
-        Plotting all queries simultaneously is not recommended as it would overload the plot.
-        - In:
-            * feats: Features Tensor of dimension [ARGS.nb_ways, ARGS.nb_shots+ARGS.nb_queries, 2].
-            * run_classes: Tensor identifying which classes are used in the few shot problem.
-            * run_indices: Tensor identifying which samples are used in the few shot problem.
-            * ARGS: args parser object.
-            * query_start: starting query number.
-            * query_end: ending query number.
-            * plot_support: boolean value, if True, plots the support points too.
-            * crops: boolean value, if plot using crops or not.
-            * figsize: figure size default is (25, 15).
-    """
-    plt.figure(figsize=figsize)
-    colors = ['red', 'deepskyblue', 'orange', 'green']
-    markers=['P', '>','^', 'D', 'd', '*', '1', '2', '3', '4', '|', 'D', 'd', '.', '*', '1', 'D', 'd', '.', '*', '1']
-
-    assert query_start<ARGS.nb_queries, f'query_start should be smaller than {ARGS.nb_queries}'
-    assert query_start>=0, f'query_start should be larger than 0'
-    assert query_end<=ARGS.nb_queries, f'query_end should be smaller than {ARGS.nb_queries}'
-    assert query_end>0, f'query_end should be larger than 0'
-
-    # Plot grid
-    plot_grid = compute_hyperplans_and_grid(reduced_features, ARGS, crops=crops)
-
-    if len(reduced_features.shape) == 3: reduced_features = reduced_features.unsqueeze(2)
-
-    # Support
-    for c in range(reduced_features.shape[0]):
-        support = reduced_features[c, :ARGS.nb_shots]
-        support_indices = [i.item() for i in run_indices[c][:ARGS.nb_shots]]
-        if plot_support:
-            for s in range(ARGS.nb_shots):
-                support_mean = support[s].mean(dim=0)
-                plt.scatter(support[s][:, 0].cpu(), support[s][:, 1].cpu(), color=colors[c], edgecolor='black', s=300, linewidth=2, marker=markers[s], label=f'C={run_classes[c]} | s={support_indices[s]}[support]', zorder=1)
-                plt.scatter(support_mean[0].cpu(), support_mean[1].cpu(), color=colors[c], edgecolor='black', s=300, linewidth=2, marker=markers[s], zorder=1)
-    # Queries
-    for c in range(reduced_features.shape[0]):
-        query = reduced_features[c, ARGS.nb_shots:]
-        query_indices = [i.item() for i in run_indices[c][ARGS.nb_shots:]]
-        for q in range(min(query.shape[0], query_start),min(query.shape[0], query_end)):
-            query_mean = query[q].mean(dim=0)
-            plt.scatter(query[q][:, 0].cpu(), query[q][: , 1].cpu(), label=f'C={run_classes[c]} | s={query_indices[q]}[query]', color=colors[c], edgecolor=colors[c], s=100, marker=markers[ARGS.nb_shots+q-query_start])
-            plt.scatter(query_mean[0].cpu(), query_mean[1].cpu(), color=colors[c], s=300, marker=markers[ARGS.nb_shots+q-query_start], linewidth=4)
-
-    # Plot Grid
-    xx, yy, Z = plot_grid['xx'], plot_grid['yy'], plot_grid['Z']
-    plt.contourf(xx.cpu(), yy.cpu(), Z.cpu(), alpha=0.1)
-
-    # Plot hyperplans
-    hyperplans, intersection = plot_grid['hyperplans'], plot_grid['intersection']
-    ymingrid, ymaxgrid, xmingrid, xmaxgrid = plot_grid['ymingrid'], plot_grid['ymaxgrid'], plot_grid['xmingrid'], plot_grid['xmaxgrid']
-
-    for i, h in enumerate(hyperplans[:]):
-        a,b, middle = h
-        if a!= np.inf and a!=0:
-            if intersection[0]>middle[0]: #intersect with left grid
-                plt.plot([intersection[0], xmingrid], [intersection[1], a*xmingrid+b], color=colors[i])
-            else:
-                plt.plot([intersection[0], xmaxgrid], [intersection[1], a*xmaxgrid+b], color=colors[i])
-        if a==np.inf:
-            if middle[1].item()<intersection[1]:
-                plt.plot([b, b], [ymingrid, intersection[1]], color=colors[i])
-            else:
-                plt.plot([b, b], [intersection[1], ymaxgrid], color=colors[i])
-
-        if a==0: #b is y
-            if middle[0].item()<intersection[0]:
-                plt.plot([xmingrid, intersection[0]], [b, b], color=colors[i])
-            else:
-                plt.plot([intersection[0], xmaxgrid], [b,b], color=colors[i])
-
-    plt.legend()
-    plt.xlim(xmingrid, xmaxgrid)
-    plt.ylim(ymingrid, ymaxgrid)
-    plt.show()
-
-def convert_label_to_color(labels, colors):
-    """
-    Convert label to color
-    """
-    if len(labels.shape)>1:
-        labels = labels.reshape(-1)
-    return [colors[l] for l in labels]
-
-def clip(x):
-    """
-    Normalize images between 0 and 255
-    """
-    return (255*(x-x.min())/(x.max()-x.min())).int()
 
 def opencsv(filename):
     """
@@ -216,50 +115,25 @@ def return_annotation_boxes(classe, sample, n_augmentation, xybox, args, crops=F
     """
     Returns annotation boxes given a classe and a sample
     """
-    if crops:
-        images = torch.load(os.path.join(features_path, 'run1.pt'), map_location='cpu')[classe, sample]
-        list_image_hover = []
-        for n in range(n_augmentation):
-            img_array = images[n]
-            im = OffsetImage(img_array, zoom=1)
-            annotBox = AnnotationBbox(im, (0,0), xybox=xybox, xycoords='data',boxcoords="offset points",  pad=0.3,  arrowprops=dict(arrowstyle="->"))
-            list_image_hover.append(annotBox)
-        return list_image_hover
-    else:
-        img_array = np.array(getimg(classe, sample, filepath=filepath, directory=directory))
-        im = OffsetImage(img_array, zoom=1)
-        annotBox = AnnotationBbox(im, (0,0), xybox=xybox, xycoords='data',boxcoords="offset points",  pad=0.3,  arrowprops=dict(arrowstyle="->"))
-        return [annotBox]
+    img_array = np.array(getimg(classe, sample, filepath=filepath, directory=directory))
+    im = OffsetImage(img_array, zoom=1)
+    annotBox = AnnotationBbox(im, (0,0), xybox=xybox, xycoords='data',boxcoords="offset points",  pad=0.3,  arrowprops=dict(arrowstyle="->"))
+    return [annotBox]
 
-def hover_few_shot_space(reduced_features, run_classes, run_indices, ARGS, figsize=(25, 15), query_start=0, query_end=2, plot_support=True, crops=False, images_path='./images/'):
-    """
-        Visualize the feature space. Allows to plot multiple queries at the same time.
-        Plotting all queries simultaneously is not recommended as it would overload the plot.
-        - In:
-            * feats: Features Tensor of dimension [ARGS.nb_ways, ARGS.nb_shots+ARGS.nb_queries, 2].
-            * run_classes: Tensor identifying which classes are used in the few shot problem.
-            * run_indices: Tensor identifying which samples are used in the few shot problem.
-            * ARGS: args parser object.
-            * query_start: starting query number.
-            * query_end: ending query number.
-            * plot_support: boolean value, if True, plots the support points too.
-            * crops: boolean value, if plot using crops or not.
-            * figsize: figure size default is (25, 15).
-            * images_path: Path of images
-    """
+def hover_few_shot_space(reduced_features, run_classes, run_indices, args, figsize=(25, 15), query_start=0, query_end=2, plot_support=True, crops=False, images_path='./images/'):
+
     fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111)
     colors = ['red', 'deepskyblue', 'orange', 'green']
-    markers=['P', '>','^', 'D', 'd', '.', '*', '1', '2', '3', '4', '|', 'D', 'd', '.', '*', '1', 'D', 'd', '.', '*', '1']
     xybox=(25., 25.)
 
-    assert query_start<ARGS.nb_queries, f'query_start should be smaller than {ARGS.nb_queries}'
+    assert query_start<args.nb_queries, f'query_start should be smaller than {args.nb_queries}'
     assert query_start>=0, f'query_start should be larger than 0'
-    assert query_end<=ARGS.nb_queries, f'query_end should be smaller than {ARGS.nb_queries}'
+    assert query_end<=args.nb_queries, f'query_end should be smaller than {args.nb_queries}'
     assert query_end>0, f'query_end should be larger than 0'
 
     # Plot Grid
-    plot_grid = compute_hyperplans_and_grid(reduced_features, ARGS, crops=crops)
+    plot_grid = compute_hyperplans_and_grid(reduced_features, args, crops=crops)
     xx, yy, Z = plot_grid['xx'], plot_grid['yy'], plot_grid['Z']
     plt.contourf(xx.cpu(), yy.cpu(), Z.cpu(), alpha=0.1)
 
@@ -292,35 +166,27 @@ def hover_few_shot_space(reduced_features, run_classes, run_indices, ARGS, figsi
 
     # Support
     for c in range(reduced_features.shape[0]):
-        support = reduced_features[c, :ARGS.nb_shots]
-        support_indices = [i.item() for i in run_indices[c][:ARGS.nb_shots]]
+        support = reduced_features[c, :args.nb_shots]
         if plot_support:
-            for s in range(ARGS.nb_shots):
+            for s in range(args.nb_shots):
                 X.append(support[s, :, 0].cpu())
                 Y.append(support[s, :, 1].cpu())
                 colors_list = colors_list+ [colors[c]]*len(support[s])
                 edgecolor_list = edgecolor_list+ ['black']*len(support[s])
-                if crops :
-                    c_ind, s_ind = c, s
-                else:
-                    c_ind, s_ind = run_classes[c], run_indices[c][s]
+                c_ind, s_ind = run_classes[c], run_indices[c][s]
 
-                list_image_hover = list_image_hover + return_annotation_boxes(c_ind, s_ind, 50, xybox, ARGS, features_path=images_path, crops=crops)
+                list_image_hover = list_image_hover + return_annotation_boxes(c_ind, s_ind, 50, xybox, args, features_path=images_path, crops=crops)
 
-        query = reduced_features[c, ARGS.nb_shots:]
-        query_indices = [i.item() for i in run_indices[c][ARGS.nb_shots:]]
-
-        for q in range(min(query.shape[0], query_start),min(query.shape[0], query_end)):
+        query = reduced_features[c, args.nb_shots:]
+        print("query: ", query.shape)
+        for q in range(query.shape[0]):
             X.append(query[q, :, 0].cpu())
             Y.append(query[q, :, 1].cpu())
             colors_list = colors_list+ [colors[c]]*len(query[q])
             edgecolor_list = edgecolor_list+ [colors[c]]*len(query[q])
-            if crops :
-                c_ind, q_ind = c, ARGS.nb_shots+q
-            else:
-                c_ind, q_ind = run_classes[c], run_indices[c][q+ARGS.nb_shots]
+            c_ind, q_ind = run_classes[c], run_indices[c][q+args.nb_shots]
 
-            list_image_hover = list_image_hover + return_annotation_boxes(c_ind, q_ind, 50, xybox, ARGS, features_path=images_path, crops=crops)
+            list_image_hover = list_image_hover + return_annotation_boxes(c_ind, q_ind, 50, xybox, args, features_path=images_path, crops=crops)
 
     X = torch.cat(X)
     Y = torch.cat(Y)
